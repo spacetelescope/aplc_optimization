57433c9a14f3177875751b6bc261c7f7c8e5855e
Emiel Por <por@strw.leidenuniv.nl>
2019-03-25 16:23:38 -0400

    Initial version for asymmetries in an APLC

--- aplc.py
+++ aplc.py
@@ -1,132 +1,260 @@
+import matplotlib as mpl
+mpl.use('Agg')
 from hcipy import *
 import numpy as np
 import matplotlib.pyplot as plt
 import gurobipy as gp
 from scipy.ndimage.morphology import grey_erosion, grey_dilation
 
-pupil_grid = make_pupil_grid(512)
-focal_grid = CartesianGrid(SeparatedCoords((np.arange(3.5, 14, 0.5), np.arange(-14, 14.1, 0.5))))
-
-fraun_prop = FraunhoferPropagator(pupil_grid, focal_grid)
-
-aperture = make_obstructed_circular_aperture(1, 0.2, 3, 0.01)
-aperture = evaluate_supersampled(aperture, pupil_grid, 4)
-
-small_focal_grid = make_focal_grid(pupil_grid, 16, 3)
-focal_plane_mask = 1 - circular_aperture(3)(small_focal_grid)
-
-lyot_stop = make_obstructed_circular_aperture(0.95, 0.3, 3, 0.02)
-lyot_stop = evaluate_supersampled(lyot_stop, pupil_grid, 4)
-
-coro = LyotCoronagraph(pupil_grid, focal_plane_mask, lyot_stop)
-
-coro_prop = OpticalSystem([coro, fraun_prop])
-
-def optimize_at_scale(pupil_grid, focal_grid, prop, aperture, lyot_stop, last_optim, subsampling, contrast):
-	inds = np.arange(pupil_grid.size).reshape((pupil_grid.shape[1]//subsampling, subsampling, pupil_grid.shape[0]//subsampling, subsampling))
-	inds = np.swapaxes(inds, 1, 2).reshape((pupil_grid.size//(subsampling**2), -1))
-
-	last_optim = np.repeat(np.repeat(last_optim.shaped, 2, 1), 2, 0).ravel()
-	aperture_subsampled = subsample_field(aperture, subsampling)
-	lyot_stop_subsampled = subsample_field(lyot_stop, subsampling)
-	last_optim = Field(last_optim, aperture_subsampled.grid)
-
-	mat = []
-	base_electric_field = np.zeros(focal_grid.size, dtype='complex')
-	optimize_mask = []
-
+def calculate_pixels_to_optimize(last_optim, pupil_subsampled):
 	structure = np.array([[0,1,0],[1,1,1],[0,1,0]])
+	#structure = np.array([[0,1,1,0],[1,1,1,1],[1,1,1,1],[0,1,1,0]])
+	#structure = np.array([[0,1,1,1,0],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[0,1,1,1,0]])
 
-	pixels_to_optimize = (grey_dilation(last_optim.shaped, structure=structure) - grey_erosion(last_optim.shaped, structure=structure)).ravel() - 2
-	pixels_to_optimize = Field(pixels_to_optimize, last_optim.grid)
+	a = (grey_dilation(last_optim.shaped, structure=structure) - grey_erosion(last_optim.shaped, structure=structure)).ravel() - 2
+	a = np.abs(a) > 1e-3
+	
+	b = np.logical_and(last_optim < (1 - 1e-3), last_optim > 1e-3)
 
-	#imshow_field(pixels_to_optimize)
-	#plt.show()
+	c = np.logical_or(a, b)
 
-	pixels_to_optimize = np.abs(pixels_to_optimize) > 1e-3
+	return np.logical_and(c, pupil_subsampled > 0)
 
+def optimize_aplc(pupil, focal_plane_mask, lyot_stops, dark_zone_mask, wavelengths, contrast, num_scalings=1):
+	pupil_grid = pupil.grid
+	focal_grid = dark_zone_mask.grid
 
-	pixels_to_optimize2 = np.logical_and(last_optim < (1 - 1e-3), last_optim > 1e-3)
+	aplc = LyotCoronagraph(pupil_grid, focal_plane_mask)
+	prop = FraunhoferPropagator(pupil_grid, focal_grid)
 
-	pixels_to_optimize = np.logical_or(pixels_to_optimize, pixels_to_optimize2)
+	last_optim = None
+	subsamplings = 2**np.arange(num_scalings)[::-1]
 
-	imshow_field(pixels_to_optimize, cmap='Reds', mask=aperture_subsampled, vmin=0, vmax=1.5)
-	plt.savefig('pixels_to_optimize%d.pdf' % subsampling)
-	plt.clf()
+	# Determine pupil symmetries
+	x_symm_pupil = np.allclose(pupil.shaped[:,::-1], pupil.shaped)
+	y_symm_pupil = np.allclose(pupil.shaped[::-1,:], pupil.shaped)
 
-	x0 = Field(np.zeros(pupil_grid.size), pupil_grid)
-
-	print('Calculating mask for %d/%d variables.' % (np.sum(pixels_to_optimize * (aperture_subsampled > 0)), np.sum(aperture_subsampled > 0)))
-
-	for ind, amp, to_optimize in zip(inds, last_optim, pixels_to_optimize):
-		if np.sum(aperture[ind]) < 1e-3:
-			optimize_mask.append(False)
+	if x_symm_pupil:
+		if y_symm_pupil:
+			print('Telescope pupil is both x- and y-symmetric.')
 		else:
-			x = Field(np.zeros(pupil_grid.size), pupil_grid)
-			x[ind] = aperture[ind]
-			
-			if not to_optimize:
-				x0 += x * amp
-				optimize_mask.append(False)
+			print('Telescope pupil is x-symmetric.')
+	elif y_symm_pupil:
+		print('Telescope pupil is y-symmetric.')
+	else:
+		print('Telescope pupil contains no mirror symmetries.')
+	
+	# Determine lyot stop symmetries
+	x_symm_lyot_stops = [np.allclose(lyot_stop.shaped[:,::-1], lyot_stop.shaped) for lyot_stop in lyot_stops]
+	y_symm_lyot_stops = [np.allclose(lyot_stop.shaped[::-1,:], lyot_stop.shaped) for lyot_stop in lyot_stops]
+
+	# Determine optimization problem symmetries
+	if x_symm_pupil:
+		if np.all(x_symm_lyot_stops):
+			x_symm = True
+		else:
+			x_symm = True
+			for a in lyot_stops:
+				if not np.any([np.allclose(a[:,::-1], b) for b in lyot_stops]):
+					x_symm = False
+	else:
+		x_symm = False
+	
+	if y_symm_pupil:
+		if np.all(y_symm_lyot_stops):
+			y_symm = True
+		else:
+			y_symm = True
+			for a in lyot_stops:
+				if not np.any([np.allclose(a[::-1,:], b) for b in lyot_stops]):
+					y_symm = False
+	else:
+		y_symm = False
+
+	# x_symm and y_symm now the symmetry of the total problem, which affects the number of 
+	# variables that need to be taken into account
+
+	# TODO: still need to remove redundant Lyot stops; fill in lyot_stop_duplication array!
+	
+	# We are optimizing amplitude (=real part) only
+	dark_zone_mask *= focal_grid.x > 0
+
+	if x_symm or y_symm:
+		dark_zone_mask *= focal_grid.y > 0
+
+	for subsampling in subsamplings:
+		pupil_subsampled = subsample_field(pupil, subsampling)
+		lyot_stops_subsampled = [subsample_field(lyot_stop, subsampling) for lyot_stop in lyot_stops]
+		pupil_grid_subsampled = pupil_subsampled.grid
+
+		# Calculate which pixels belong to which superpixel
+		inds = np.arange(pupil_grid.size).reshape((pupil_grid.shape[1]//subsampling, subsampling, pupil_grid.shape[0]//subsampling, subsampling))
+		inds = np.swapaxes(inds, 1, 2)#.reshape((pupil_grid.size//(subsampling**2), -1))
+
+		# Apply x,y-mirror-symmetries
+		mask = Field(np.ones(pupil_grid_subsampled.size), pupil_grid_subsampled)
+		if x_symm:
+			inds = np.concatenate(inds, inds[:,::-1,:])
+			mask *= pupil_grid_subsampled.x > 0
+		if y_symm:
+			inds = np.concatenate(inds, inds[::-1,:,:])
+			mask *= pupil_grid_subsampled.y > 0
+		
+		mask = mask.astype('bool')
+		inds = inds.reshape((pupil_grid_subsampled.size, -1))
+		inds = [inds[i,:] for i in range(pupil_grid_subsampled.size) if mask[i]]
+
+		# Upscale last optim to current resolution
+		blind = last_optim is None
+		if blind:
+			# No prior information; assume totally dark apodizer
+			last_optim = Field(np.zeros(pupil_grid_subsampled.size), pupil_grid_subsampled)
+		else:
+			# Upscale prior information by factor 2
+			last_optim = np.repeat(np.repeat(last_optim.shaped, 2, 1), 2, 0).ravel()
+			last_optim = Field(last_optim, pupil_grid_subsampled)
+		
+		# Get pixels to optimize
+		optimize_mask = calculate_pixels_to_optimize(last_optim, pupil_subsampled)[mask]
+		if blind:
+			optimize_mask[:] = pupil_subsampled > 0
+		n = int(np.sum(optimize_mask))
+
+		# Create Gurobi model
+		model = gp.Model('lp')
+		model.Params.Threads = 0
+		model.Params.Crossover = 0
+		model.Params.Method = 2
+		x_vars = model.addVars(n, lb=0, ub=1)
+
+		# Find number of constraints
+		num_constraints_per_focal_point = []
+		for i, lyot_stop in enumerate(lyot_stops):
+			if lyot_stop_duplication[i]:
+				# Lyot stop already taken into account due to symmetries
+				num_constraints_per_focal_point.append(0)
+			elif (x_symm_pupil and x_symm_lyot_stops[i]) and (y_symm_pupil and y_symm_lyot_stops[i]):
+				# Only constrain real part
+				num_constraints_per_focal_point.append(1)
 			else:
-				y = prop(Wavefront(x)).electric_field
-				mat.append(y)
-				optimize_mask.append(True)
-				if np.sum(optimize_mask) % 1000 == 0:
-					print(np.sum(optimize_mask))
+				# Constrain both real and imag part
+				num_constraints_per_focal_point.append(2)
+		mm = int(np.sum(dark_zone_mask))
+		m = int(np.sum(num_constraints_per_focal_point)) * mm
 
-	optimize_mask = np.array(optimize_mask)
-	base_electric_field = prop(Wavefront(x0)).electric_field
+		# Create problem matrix for one wavelength but for all Lyot stops
+		M = np.empty((m, n))
 
-	M = np.array(mat).T
-	M = np.vstack([M.real, M.imag])
+		# Add constraints for each wavelength
+		for wavelength in wavelengths:
+			j = 0
+			x0 = Field(np.zeros(pupil_grid.size), pupil_grid)
+			x = Field(np.zeros(pupil_grid.size), pupil_grid)
+			
+			for ind, amp, to_optimize in zip(inds, last_optim, optimize_mask):
+				x[:] = 0
+				x[ind] = pupil[ind]
 
-	M_max = (aperture_subsampled * lyot_stop_subsampled)[optimize_mask]
+				if not to_optimize:
+					# Add to accumulator
+					x0 += x * amp
+				else:
+					# Calculate field before the Lyot stop
+					lyot = aplc(Wavefront(x, wavelength))
 
-	E_0 = np.concatenate((base_electric_field.real, base_electric_field.imag))
+					k = 0
+					for i, lyot_stop in enumerate(lyot_stops):
+						if num_constraints_per_focal_point[i] == 0:
+							continue
+						
+						# Apply the Lyot stop and get focal-plane electric field
+						lyot_copy = lyot.copy()
+						lyot_copy.electric_field *= lyot_stop
+						E = prop(lyot_copy).electric_field[dark_zone_mask]
 
-	m, n = M.shape
-	print(m, n)
+						# Add only imaginary or both imaginary and real constraints depending on symmetry
+						M[k*mm:(k+1)*mm,j] = E.imag
+						if num_constraints_per_focal_point[i] == 2:
+							M[(k+1)*mm:(k+2)*mm,j] = E.real
+						
+						k += num_constraints_per_focal_point[i]
+					j += 1
 
-	contrast_requirement = np.ones(m) * np.sqrt(contrast)
+			# Calculate base electric field
+			base_electric_field = []
+			for i, lyot_stop in enumerate(lyot_stops):
+				if num_constraints_per_focal_point[i] == 0:
+					continue
+				
+				wf = aplc(Wavefront(x0, wavelength))
+				wf.electric_field *= lyot_stop
+				img = prop(wf)
+				
+				base_electric_field.append(img.electric_field.imag)
+				if num_constraints_per_focal_point[i] == 2:
+					base_electric_field.append(img.electric_field.real)
+			base_electric_field = np.concatenate(base_electric_field)
 
-	model = gp.Model('lp')
-	model.Params.Threads = 4
+			# Calculate contrast requirement
+			contrast_requirement = np.tile([np.ones(m) * np.sqrt(contrast[dark_zone_mask])], int(np.sum(num_constraints_per_focal_point)))
 
-	x = model.addVars(n, lb=0, ub=1)
+			# Add constraints
+			for ee, e0, c0 in zip(M, base_electric_field, contrast_requirement):
+				e = gp.LinExpr(ee, x_vars.values())
+				model.addConstr(e <= (-e0 + c0))
+				model.addConstr(e >= (-e0 - c0))
+		
+		del M
 
-	obj = gp.quicksum((x[i] * M_max[i] for i in range(n)))
-	model.setObjective(obj, gp.GRB.MAXIMIZE)
+		# Use central Lyot stop for throughput metric (assume that this is the unshifted Lyot stop)
+		M_max = (pupil_subsampled * lyot_stops_subsampled[0])
+		obj = gp.LinExpr(M_max, x_vars.values())
+		model.setObjective(obj, gp.GRB.MAXIMIZE)
 
-	for i, ee in enumerate(M):
-		e = gp.quicksum((x[i] * ee[i] for i in range(n)))
-		model.addConstr(e <= -E_0[i] + contrast_requirement[i])
-		model.addConstr(e >= -E_0[i] - contrast_requirement[i])
+		# Optimize model
+		model.optimize()
+
+		# Extract solution from Gurobi
+		solution = np.array([x_vars[i].x for i in range(n)])
+
+		# Integrate solution into upsampled old solution
+		sol = last_optim
+		sol[optimize_mask] = solution
+		sol = Field(sol, make_subsampled_grid(pupil_grid, subsampling))
+		
+		last_optim = sol
 	
-	del M
+if __name__ == '__main__':
+	contrast = 1e-8
+	num_pix = 486
+	q_sci = 2 # px / (lambda_0/D)
+	iwa = 3.75 # lambda_0/D
+	owa = 15 # lambda_0/D
+	n_foc = 50 # px diameter
+	foc_inner = 8.543 # lambda_0/D diameter
+	spectral_bandwidth = 0.1 # fractional
+	num_wavelengths = 3
 	
-	model.optimize()
+	testing = True
 
-	solution = np.array([x[i].x for i in range(n)])
+	if testing:
+		num_pix = 256
+		owa = 8
+		num_wavelengths = 2
 
-	sol = last_optim
-	sol[optimize_mask] = solution
-	sol = Field(sol, make_subsampled_grid(pupil_grid, subsampling))
-	imshow_field(sol, cmap='gray', mask=aperture_subsampled)
-	plt.savefig('aplc_%d.pdf' % subsampling)
-	plt.clf()
+	pupil_grid = make_pupil_grid(num_pix)
+	
+	n_sci = int((ceil(owa) + 1) * q_sci) * 2
+	x_sci = (np.arange(n_sci) + 0.5 - n_sci / 2) / q_sci
+	focal_grid = CartesianGrid(SeparatedCoords((x_sci, x_sci)))
 
-	return sol
+	dark_zone_mask = circular_aperture(owa * 2)(focal_grid) - circular_aperture(iwa * 2)(focal_grid)
 
-	print(M.sum())
-	print(M_max.shape)
-	return
-	m, n = M.shape
-	print(m, n)
+	q_foc = foc_inner / n_foc
+	x_foc = (np.arange(n_foc) + 0.5 - n_foc / 2) / q_foc
+	focal_mask_grid = CartesianGrid(SeparatedCoords((x_foc, x_foc)))
 
-g = make_subsampled_grid(pupil_grid, 16)
-last_optim = Field(np.ones(g.size) * 0.5, g)
-contrast = 1e-10
+	focal_plane_mask = circular_aperture(foc_inner)(focal_mask_grid)
 
-for subsampling in [8,4,2,1]:
-	last_optim = optimize_at_scale(pupil_grid, focal_grid, coro_prop, aperture, lyot_stop, last_optim, subsampling, contrast)
+	imshow_field(focal_plane_mask)
+	plt.show()
