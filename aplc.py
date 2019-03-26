ac846e6d2ca3f4aa3f2b526c5b90655b64a9dd4e
Emiel Por <por@strw.leidenuniv.nl>
2019-03-25 20:43:44 -0400

    More bugfixes.

--- aplc.py
+++ aplc.py
@@ -1,9 +1,9 @@
-import matplotlib as mpl
-mpl.use('Agg')
+#import matplotlib as mpl
+#mpl.use('Agg')
 from hcipy import *
 import numpy as np
 import matplotlib.pyplot as plt
-import gurobipy as gp
+#import gurobipy as gp
 from scipy.ndimage.morphology import grey_erosion, grey_dilation
 
 def calculate_pixels_to_optimize(last_optim, pupil_subsampled):
@@ -143,20 +143,20 @@ def optimize_aplc(pupil, focal_plane_mask, lyot_stops, dark_zone_mask, wavelengt
 
 		# Calculate which pixels belong to which superpixel
 		inds = np.arange(pupil_grid.size).reshape((pupil_grid.shape[1]//subsampling, subsampling, pupil_grid.shape[0]//subsampling, subsampling))
-		inds = np.swapaxes(inds, 1, 2)#.reshape((pupil_grid.size//(subsampling**2), -1))
+		inds = np.swapaxes(inds, 1, 2).reshape((pupil_grid_subsampled.shape[0], pupil_grid_subsampled.shape[1], -1))#.reshape((pupil_grid.size//(subsampling**2), -1))
 
 		# Apply x,y-mirror-symmetries
 		mask = Field(np.ones(pupil_grid_subsampled.size), pupil_grid_subsampled)
 		if x_symm:
-			inds = np.concatenate((inds, inds[:,::-1,:]))
-			mask *= pupil_grid_subsampled.x > 0
+			inds = np.concatenate((inds, inds[:,::-1,:]), axis=2)
+			mask *= pupil_grid_subsampled.x < 0
 		if y_symm:
-			inds = np.concatenate((inds, inds[::-1,:,:]))
-			mask *= pupil_grid_subsampled.y > 0
+			inds = np.concatenate((inds, inds[::-1,:,:]), axis=2)
+			mask *= pupil_grid_subsampled.y < 0
 		
 		mask = mask.astype('bool')
 		inds = inds.reshape((pupil_grid_subsampled.size, -1))
-		inds = [inds[i,:] for i in range(pupil_grid_subsampled.size) if mask[i]]
+		#inds = [inds[i,:] for i in range(pupil_grid_subsampled.size) if mask[i]]
 
 		# Upscale last optim to current resolution
 		blind = last_optim is None
@@ -178,11 +178,11 @@ def optimize_aplc(pupil, focal_plane_mask, lyot_stops, dark_zone_mask, wavelengt
 		print('Creating model...')
 
 		# Create Gurobi model
-		model = gp.Model('lp')
-		model.Params.Threads = 0
-		model.Params.Crossover = 0
-		model.Params.Method = 2
-		x_vars = model.addVars(n, lb=0, ub=1)
+		#model = gp.Model('lp')
+		#model.Params.Threads = 0
+		#model.Params.Crossover = 0
+		#model.Params.Method = 2
+		#x_vars = model.addVars(n, lb=0, ub=1)
 
 		print('Calculating and adding constraints...')
 
