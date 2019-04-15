b42360efd9f1738024ea62837a1aaaabad3e3ed5
Emiel Por <por@strw.leidenuniv.nl>
2019-04-15 10:11:31 -0400

    Do lower resolution surveys

--- do_magnification_survey.py
+++ do_magnification_survey.py
@@ -1,7 +1,7 @@
 from survey import DesignParameterSurvey
 from por_aplc import PorAPLC
 
-varying_parameters = {'pupil': {'filename': 'ehpor_hicat_apodizer_mask_512_gy.fits'}, 'lyot_stop': {'filename': 'ehpor_hicat_lyot_mask_512_gy_2.fits'}, 'image': {'owa': 12}}
+varying_parameters = {'pupil': {'filename': 'ehpor_hicat_apodizer_mask_256_gy.fits'}, 'lyot_stop': {'filename': 'ehpor_hicat_lyot_mask_256_gy_2.fits'}, 'image': {'owa': 12}}
 
 survey = DesignParameterSurvey(PorAPLC, varying_parameters, 'magnfication_survey1/', 'masks/')
 survey.describe()
@@ -10,7 +10,7 @@ survey.write_drivers(False)
 survey.run_optimizations(False)
 survey.run_analyses(True)
 
-varying_parameters = {'pupil': {'filename': 'ehpor_hicat_apodizer_mask_512_gy.fits'}, 'lyot_stop': {'filename': 'ehpor_hicat_lyot_mask_512_gy_{:d}.fits', 'num_stops': 5}, 'image': {'owa': 12}}
+varying_parameters = {'pupil': {'filename': 'ehpor_hicat_apodizer_mask_256_gy.fits'}, 'lyot_stop': {'filename': 'ehpor_hicat_lyot_mask_256_gy_{:d}.fits', 'num_stops': 5}, 'image': {'owa': 12}}
 
 survey = DesignParameterSurvey(PorAPLC, varying_parameters, 'magnfication_survey5/', 'masks/')
 survey.describe()
