3236c1f4c5ee4309d4822264a1a44bfde86e4c11
Kathryn St.Laurent <kstlaurent@stsci.edu>
2019-06-20 15:59:03 -0400

    HiCAT template and inputs script done

--- HiCAT_Inputs_Generation.py
+++ HiCAT_Inputs_Generation.py
@@ -8,45 +8,51 @@ def HiCAT_inputs_gen(input_files_dict):
 	
 	# Contour
 	p3_apodizer_size = 19.725e-3 # m
+	gamma_31 = 1.008
+	gamma_51 = 0.979
 	
 	filepath    = input_files_dict['directory']
 	N           = input_files_dict['N']
 	ap_spid     = input_files_dict['aperture']['ap_spid']
 	ap_gap      = input_files_dict['aperture']['ap_gap']
-	ap_grey		= input_files_dict['aperture']['ap_grey']   	
-	lyot_ref_diam = input_files_dict['lyot_stop']['lyot_ref_diam']
-	LS_OD         = input_files_dict['lyot_stop']['LS_OD']
-	LS_ID         = input_files_dict['lyot_stop']['LS_ID']	
+	ap_grey		= input_files_dict['aperture']['ap_grey'] 
+	pup_diam	= input_files_dict['aperture']['pup_diam']   	
+	
+	ls_spid		= input_files_dict['lyot_stop']['ls_spid'] 
+	ls_grey		= input_files_dict['lyot_stop']['ls_grey'] 
+	LS_OD       = input_files_dict['lyot_stop']['LS_OD']
+	LS_ID       = input_files_dict['lyot_stop']['LS_ID']	
 	
 	if ap_grey:
 		oversamp    = 4
 	else:
-		oversamp    = 4
+		oversamp    = 1
 	
 	pup_filename = filepath+'ApodMask_HiCAT_ovsamp{0:02d}_N{1:04d}_Spider{2:}_Gap{3:}.fits'.format(oversamp, N,ap_spid,ap_gap)
-	
+		
 	grid                        = make_uniform_grid(N, [p3_apodizer_size, p3_apodizer_size])
 	
 	config = Path('masks/'+pup_filename)
 	if config.is_file():
 		print('{0:s} exists'.format('masks/'+pup_filename))
 	else:
-		HiCAT_ap, header           = make_hicat_aperture(normalized=True, with_spiders=ap_spid, with_segment_gaps=ap_gap)
+		HiCAT_ap, header           = make_hicat_aperture(with_spiders=ap_spid, with_segment_gaps=ap_gap)
+		
 		pupil                      = evaluate_supersampled(HiCAT_ap,grid,oversamp)
 	
 		
 		hdr = fits.Header()
 		hdr.set('TELESCOP', header['TELESCOP'])
-		hdr.set('D_CIRC', header['D_CIRC'],'m: circumscribed diameter')
-		hdr.set('D_SEG_CIRC',header['D_SEG_CIRC'],'m: segment circumscribed diameter')
+		hdr.set('P3_APOD', header['P3_APOD'],'m: p3 apodizer size')
+		hdr.set('P3_CENT_SEG',header['P3_CENT_SEG'],'m: p3 apodizer mask central segment')
 		
 		if ap_gap:
-			hdr.set('SEG_GAP',header['SEG_GAP'],'m: actual gap size between segments')
+			hdr.set('P3_GAP',header['P3_GAP'],'m: actual gap size between segments in P3')
+			hdr.set('P3_GAP_OVER',header['P3_GAP_OVER'],'apod gap oversize factor wrt irisao ')
+			
 		if ap_spid:
-			hdr.set('STRUT_W',header['STRUT_W'],'m: actual support strut width')
+			hdr.set('P3_STRUT',header['P3_STRUT'],'m: P3 apodizer mask spiders thickness')
 		
-		hdr.set('NORM',header['NORM'],'normalization keyword, OD scaled to 1 by Dcirc')
-		hdr.set('SEG_TRAN',header['SEG_TRAN'],'The transmission for each of the segments')
 		hdr.set('PROV',header['PROV'])
 	
 		fits.writeto('masks/'+pup_filename, pupil.shaped, hdr,overwrite=True)
@@ -54,42 +60,39 @@ def HiCAT_inputs_gen(input_files_dict):
 		
 	ls_filenames = []
 	
+	if ls_grey:
+		oversamp    = 4
+	else:
+		oversamp    = 1
+	
 	for ls_od in LS_OD:
 		for ls_id in LS_ID:
 
-			if ap_spid:
-				ls_filename  = filepath+'LS_HiCAT_ID{0:04d}_OD{1:04d}_Spider{2:}_ovsamp{3:02d}_N{4:04d}.fits'.format(int((ls_id/lyot_ref_diam)*1000),\
-                                                                                    								int((ls_od/lyot_ref_diam)*1000), \
-                                                                                        							ap_spid,oversamp, N)
+			ls_filename  = filepath+'LS_HiCAT_ID{0:04d}_OD{1:04d}_Spider{2:}_ovsamp{3:02d}_N{4:04d}.fits'.format(int((ls_id/pup_diam)*1000),\
+                                                                                    								int((ls_od/pup_diam)*1000), \
+                                                                                        							ls_spid,oversamp, N)
             
-			ls_filename  = filepath+'LS_HiCAT_ID{0:04d}_OD{1:04d}_ovsamp{2:02d}_N{2:04d}.fits'.format(int((ls_id/lyot_ref_diam)*1000),\
-                                                                                        int((ls_od/lyot_ref_diam)*1000), \
-                                                                                        oversamp, N)
-
 			config = Path('masks/'+ls_filename)
 			if config.is_file():
 				print('{0:s} exists'.format('masks/'+ls_filename))
 				
 			else:
 							
-				LUVOIR_ls, ls_header = make_hicat_lyot_stop(lyot_ref_diam, ls_id, ls_od,normalized=True, with_spiders=ap_spid)
-				lyot_stop = evaluate_supersampled(LUVOIR_ls, grid, oversamp)
+				LUVOIR_ls, ls_header = make_hicat_lyot_stop(ls_id, ls_od, with_spiders=ls_spid)
+				lyot_stop = evaluate_supersampled(LUVOIR_ls, grid.scaled(gamma_51 / gamma_31), oversamp)
 				
 				hdr = fits.Header()
 				ls_header['OVERSAMP'] = oversamp
 				  		
 				hdr.set('TELESCOP', ls_header['TELESCOP'])
-				hdr.set('D_CIRC', ls_header['D_CIRC'],'m: circumscribed diameter')
-				hdr.set('LS_REF_D',ls_header['LS_REF_D'],'m: used to reference given LS id and od')
-				hdr.set('LS_ID', ls_header['LS_ID'], 'LS inner d, fraction of LS_REF_D')
-				hdr.set('LS_OD', ls_header['LS_OD'], 'LS outer d, fraction of LS_REF_D')
+				hdr.set('P3_APOD', ls_header['P3_APOD'],'m: p3 apodizer size')
+				hdr.set('P3_CENT_SEG',ls_header['P3_CENT_SEG'],'m: p3 apodizer mask central segment')
+				hdr.set('LS_CENT', ls_header['LS_CENT'], 'm p5 ls mask central segment size')
+				hdr.set('LS_SIZE', ls_header['LS_SIZE'], 'm p5 ls size')
 				
-				if ap_spid:
-					hdr.set('STRUT_W',ls_header['LS_STRUT_W'],'m: actual support strut width')
-	
-				hdr.set('NORM',ls_header['NORM'],'normalization keyword, OD scaled to 1 by Dcirc')
-				hdr.set('OVERSAMP',ls_header['OVERSAMP'],'oversampling factor, # grey levels')
-
+				if ls_spid:
+					hdr.set('P5_STRUT',ls_header['P5_STRUT'],'m: P5 ls mask spiders thickness')
+				
 				fits.writeto('masks/'+ls_filename, lyot_stop.shaped, hdr,overwrite=True)
 				print('{0:s} has been written to file'.format('masks/'+ls_filename))
 				
--- do_hicat_template.py
+++ do_hicat_template.py
@@ -6,27 +6,24 @@ from HiCAT_Inputs_Generation import HiCAT_inputs_gen
 
 n = 486
 
-FPM1 = 1
-FPM2 = 2
+FPM1 = 8.543/2 # lambda_0/D radius
+FPM2 = 6.271/2 # lambda_0/D radius
 
-pupil_diameter = 19.9e-3 #m
+pupil_diameter = 19.9e-3 #m P3 apodizer size
+lyot_inner = 6.8e-3 #m p5 lyot stop mask central segment size
+lyot_outer = 15.9e-3 #m p5 lyot stop size
 
-lyot_inner = 6.8e-3
-lyot_outer = 15.9e-3
 
-
-input_files_dict = {'directory':'HiCAT/', 'N':n\
-					'aperture': {'ap_spid':True,'ap_gap':True,'ap_grey':True}, \
-					'lyot_stop':{'lyot_ref_diam':pupil_diameter,'LS_ID':[lyot_inner], 'LS_OD':[lyot_outer]}}
+input_files_dict = {'directory':'HiCAT/', 'N':n,\
+					'aperture': {'ap_spid':True,'ap_gap':True,'ap_grey':False,'pup_diam':pupil_diameter}, \
+					'lyot_stop':{'ls_spid':True,'ls_grey':True,'LS_ID':[lyot_inner], 'LS_OD':[lyot_outer]}}
 
 pup_filename, ls_filenames = HiCAT_inputs_gen(input_files_dict)
 
-exit()
-
 survey_parameters = {'pupil': {'N': n,'filename': pup_filename}, \
-                     'lyot_stop': {'filename': ls_filenames}, \
-                     'focal_plane_mask': {'radius':FPM1, 'num_pix':150 , 'grayscale': True,},
-                     'image': {'contrast':8,'iwa':3.75,'owa':15.0,'bandwidth':0.10,'num_wavelengths':5}, \
+                     'lyot_stop': {'filename': ls_filenames,'alignment_tolerance': 4, 'num_lyot_stops': 9}, \
+                     'focal_plane_mask': {'radius':FPM1, 'num_pix': 80, 'grayscale': True,},
+                     'image': {'contrast':8,'iwa':3.75,'owa':15.0,'bandwidth':0.10,'num_wavelengths':4}, \
                      'method':{'starting_scale': 1}}
 
 
