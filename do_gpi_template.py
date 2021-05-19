b68024a94a27485aad86091f2ef584f0e89d4419
Bryony Nickson <bnickson@stsci.edu>
2021-05-19 12:55:37 -0400

    Fixing GPI template

--- do_gpi_template.py
+++ do_gpi_template.py
@@ -21,9 +21,9 @@ Workflow:
 - after survey is done, move the launcher script into the survey directory
 '''
 
-nArray = 512  # number of pixels in input and final arrays
+nArray = 128  # number of pixels in input and final arrays
 instrument = "gpi"  # instrument name
-survey_name = "fpmK1_robust_5px_5ls_iwa3_owa44"  # survey name
+survey_name = "do_template"  # survey name
 machine = "telserv3"  # machine the survey is run on.
 
 #oversamp = 2 # oversampling factor for intermediate plane calculations.
@@ -34,7 +34,7 @@ satspots = False # whether to include satellite spots grid
 ls_tabs = False # whether  block out the bad actuators with tabs in the Lyot masks
 lyot_mask = '080m12_04' # name of Lyot mask. Available lyot masks are:
                         #  '080m12_02', '080m12_03', '080m12_04', '080m12_04_c', '080m12_06',
-                        #  '080m12_06_03', '080m12_07', '080m12_10', 'Open' and 'Blank'.
+                        #  '080m12_06_03', '080mgit 12_07', '080m12_10', 'Open' and 'Blank'.
 ls_spid = True
 
 
@@ -43,8 +43,6 @@ input_files_dict = {'directory': 'GPI/', 'N': nArray,
                     'lyot_stop': {'lyot_mask':lyot_mask, 'ls_tabs': ls_tabs, 'ls_spid': ls_spid}}
 
 pup_filename, ls_filename = GPI_inputs_gen(input_files_dict)
-#pup_filename = 'GPI/GPI_primary_N0300.fits'
-#ls_filename = 'GPI/GPI_LS_ovsamp01_N0300_080m12_04_struts_notabs.fits'
 
 satspots = True # Whether to include the satellite spots in the apodizer model
 
@@ -60,14 +58,14 @@ contrast = 8 # contrast goal in the dark zone, where 10^-{contrast}
 IWA = 3 #lamda_0/D: dark zone inner radius (rho_i) - minimum distance at which the transmission of an off-axis companion is significantly high.
 OWA = 44 #lambda_0/D: dark zone outer radius (rho_o)
 spectral_bandwidth = 0.2 # fractional: spectral bandwidth over which we want to optimize
-nLams = 3 # number of wavelengths spanning the design bandpass
+nLams = 1 # number of wavelengths spanning the design bandpass
 res = 2
 # method parameters
 starting_scale = 4 # number of pixels per unit cell for the initial solution.
 ending_scale = 1
 
-alignment_tolerance = 5 #pixels
-num_lyot_stops = 5
+alignment_tolerance = 1 #pixels
+num_lyot_stops = 1
 
 survey_parameters = {'instrument': {'inst_name': instrument.upper()}, 'pupil': {'N': nArray, 'filename': pup_filename, 'satspots': True}, 'lyot_stop': {'filename': ls_filename, 'ls_tabs': True, 'alignment_tolerance': alignment_tolerance, 'num_lyot_stops': num_lyot_stops}, 'focal_plane_mask': {'FPM_name': FPM_name, 'radius': FPM_radius, 'num_pix': nArray}, 'image': {'contrast': contrast,'iwa': IWA,'owa': OWA,'bandwidth': spectral_bandwidth,'num_wavelengths':nLams, 'resolution': res}, 'method': {'starting_scale': starting_scale, 'ending_scale': ending_scale}}
 
