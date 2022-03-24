f5ac8ef5138c5d0260834ffab06814095248aee6
Bryony Nickson <bnickson@stsci.edu>
2022-03-24 19:32:07 -0400

    Adding initial 512px survey results

---
+++ optimization_launchers/SCDA/do_LUVex_survey.py
@@ -0,0 +1,180 @@
+'''This is the launcher template for the SCDA segment size survey.'''
+
+import os
+
+os.chdir('../..')
+instrument = 'scda'  # do not edit
+from aplc_optimization.survey import DesignParameterSurvey
+from aplc_optimization.aplc import APLC
+from aplc_optimization.Inputs_Generation.SCDA_Inputs_Generation import SCDA_inputs_gen
+
+'''
+Survey Information
+------------------
+survey_name: str
+    The designated name of the design survey.
+machine: str
+    The name of the machine on which to run the design survey.
+N: int
+    The number of pixels in the input (TelAP, LS) and final (apodizer) arrays.
+'''
+survey_name = 'LUVex_TestAps_survey'
+machine = 'telserv3'
+N = 1024
+
+'''
+Input File Parameters
+----------------------
+Gap padding (seg_gap_pad) and grey levels (oversamp) are set according to number of input array pixels (nArray),  
+configured in order to keep gap size as close to actual physical size of LUVOIR A, as possible. 
+
+ - N = 1000, oversamp = 4, seg_gap_pad = 1
+ - N = 500,  oversamp = 3, seg_gap_pad = 2
+ - N = 300,  oversamp = 4, seg_gap_pad = 4
+ - N = 200,  oversamp = 4, seg_gap_pad = 4
+ - N = 100,  oversamp = 4, seg_gap_pad = 4
+
+N: int
+    The number of pixels in input (TelAP, LS) and final (apodizer) arrays
+oversamp: int
+    The oversampling factor (number of grey levels) - if set to 1 will return a bw pupil, for grey set to > 1.
+gap_padding: int
+    An arbitary padding of gap size to represent gaps on smaller arrays. This effectively makes the larger gaps larger 
+    and the segments smaller to preserve the same segment pitch.
+num_rings : int
+    The number of rings of hexagons to include, not counting the central segment.
+clipped : bool
+    Remove corner segments to maximize the diameter of the inscribed circle.
+ap_spid : bool
+    Include the secondary mirror support structure in the aperture.
+ap_obs : bool
+    Include the secondary obstruction in the aperture.
+lyot_ref_diam: float 
+    The diameter used to reference the LS inner and outer diameter against.
+ls_spid: bool 
+    Include secondary support mirror structure in the aperture.
+ls_spid_ov: int
+    The factor by which to oversize the spiders compared to the LUVOIR-A aperture spiders.
+LS_ID: float
+    The Lyot stop inner diameter(s) relative to the `lyot_ref_diameter` (inscribed circle). This is re-normalized 
+    against the circumscribed pupil in the `LUVOIR_inputs_gen` function.
+LS_OD: float
+    The Lyot stop outer diameter(s) relative to the `lyot_ref_diameter` (inscribed circle). This is re-normalized 
+    against the circumscribed pupil in the `LUVOIR_inputs_gen` function.
+'''
+# Aperture parameters
+                #hex1,  hex2,    hex3,   hex4,   hex5
+pupil_diameter = 7.9445, 7.2617, 7.7231, 7.1522, 5.9941
+pupil_inscribed = 6.0023, 5.9994, 5.9899, 5.9937, 6.8526
+oversamp = 4
+seg_gap_pad = 1
+num_rings = 1
+clipped = True
+ap_spid = False
+ap_obs = False
+
+# Lyot stop parameters
+lyot_ref_diam = pupil_inscribed
+ls_spid = False
+ls_spid_ov = 2
+LS_ID = 0
+LS_OD = 0.982
+
+# INPUT FILES PARAMETER DICTIONARY
+input_files_dict = {'directory': 'SCDA/', 'N': N, 'oversamp': oversamp,
+                    'aperture': {'num_rings': num_rings, 'clipped': clipped, 'ap_spid': ap_spid, 'ap_obs': ap_obs,
+                                 'seg_gap_pad': seg_gap_pad},
+                    'lyot_stop': {'lyot_ref_diam': lyot_ref_diam, 'ls_spid': ls_spid, 'ls_spid_ov': ls_spid_ov,
+                                  'LS_ID': [LS_ID], 'LS_OD': [LS_OD]}}
+
+# INPUT FILE GENERATION
+
+# gather the pre-generated input mask files to run in survey mode
+pup_files = []
+ls_files = []
+for i in range(5):
+    if i < 2:
+        pup_filename = 'SCDA/TelAp_LUVex_{0:02d}-Hex_gy_ovsamp04__N1024.fits'.format(i + 1)
+    else:
+        pup_filename = 'SCDA/TelAp_LUVex_{0:02d}-Hex_gy_clipped_ovsamp04__N1024.fits'.format(i+1)
+
+    ls_filename = 'SCDA/LS_LUVex_{0:02d}-Hex_ID0000_OD0982_no_struts_gy_ovsamp4_N1024.fits'.format(i+1)
+    pup_files.append(pup_filename)
+    ls_files.append(ls_filename)
+
+'''
+Survey Design Parameters
+------------------------
+- for multiple design parameters as a grid, input as list
+- for multiple design parameters NOT as a grid, create multiple `survey_parameters` dictionaries
+  (as shown in the commented block, at bottom of this script).
+
+Parameters
+----------  
+radius: float  
+    The radius of the FPM in lamda_0/D.
+num_pix: float
+    The number of pixels in the focal plane mask.
+greyscale: bool
+    Whether to model a grayscale focal plane mask, else black and white.
+iwa: float
+    The effective inner working angle (outer perimeter of the annular dark zone in coronagraphic image) in lam/D.
+owa: float
+    The effective outer working angle (inner perimeter of the annular dark zone in the coronagraphic image) in lam/D.
+bandwidth: float
+    The spectral bandwidth over which to optimize (fractional).
+num_wavelengths: int
+    The number of wavelengths spanning the bandpass.
+contrast: int
+    The contrast goal in the dark zone (exponent of 10). 
+'''
+# FPM Parameters
+radius = 3.5
+num_pix = 150
+grayscale = True
+
+# Optimization parameters (dark zone constraints)
+iwa = 3.4
+owa = 12.0
+bandwidth = 0.1
+num_wavelengths = 3
+contrast = 10
+starting_scale = 4
+
+
+for i in range(5):
+    # SURVEY PARAMS DICTIONARY
+    survey_parameters = {'instrument': {'inst_name': instrument.upper()},
+                        'pupil': {'N': N, 'filename': pup_files[i]},
+                        'lyot_stop': {'filename': ls_files[i]},
+                        'focal_plane_mask': {'radius': radius, 'num_pix': num_pix, 'grayscale': grayscale},
+                        'image': {'contrast': contrast, 'iwa': iwa, 'owa': owa, 'bandwidth': bandwidth,
+                                   'num_wavelengths': num_wavelengths},
+                        'method': {'starting_scale': starting_scale}}
+
+    # RUN DESIGN SURVEY
+    SCDA = DesignParameterSurvey(APLC, survey_parameters,
+                                   'surveys/{}_{}_N{:04d}_{}/{}-Hex'.format(instrument, survey_name, N, machine, i+1), 'masks/')
+    SCDA.describe()
+    SCDA.write_drivers(True)
+    SCDA.run_optimizations(True)
+    SCDA.run_analyses(True)
+
+
+'''
+Example for multiple design parameters NOT as a grid
+########################################################
+
+survey_parameters_2 = {'pupil': {'N': n,'filename': pup_filename}, \
+                     'lyot_stop': {'filename': ls_filenames}, \
+                     'focal_plane_mask': {'radius':6.82, 'num_pix': 250, 'grayscale': True,},
+                     'image': {'contrast':10,'iwa':6.72,'owa':23.72,'bandwidth':0.10,'num_wavelengths':5}, \
+                     'method':{'starting_scale': 4}}
+
+luvoir = DesignParameterSurvey(PorAPLC, survey_parameters_2, 'surveys/luvoir_{}_small_N{:04d}_{}/'.format(survey_name,n,machine), 'masks/')
+luvoir.describe()
+
+luvoir.write_drivers(True)
+luvoir.run_optimizations(True)
+luvoir.run_analyses(True)
+'''
---
+++ surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/drivers/0_SCDA_N512_FPM350M0150_IWA0340_OWA01200_C10_BW10_Nlam3_LS_IDex_ID_OD0_OD_ls_982_no_strut.py
@@ -0,0 +1,177 @@
+parameters = {'instrument': {'inst_name': 'SCDA'}, 'pupil': {'filename': 'SCDA/TelAp_LUVex_05-Hex_gy_clipped_ovsamp03__N0512.fits', 'N': 512}, 'focal_plane_mask': {'radius': 3.5, 'num_pix': 150, 'grayscale': True, 'field_stop_radius': -1.0}, 'lyot_stop': {'filename': 'SCDA/LS_LUVex_05-Hex_ID0000_OD0982_no_struts_gy_ovsamp3_N0512.fits', 'alignment_tolerance': 0, 'num_lyot_stops': 1}, 'image': {'contrast': 10, 'iwa': 3.4, 'owa': 12.0, 'num_wavelengths': 3, 'bandwidth': 0.1, 'resolution': 2}, 'method': {'force_no_x_mirror_symmetry': False, 'force_no_y_mirror_symmetry': False, 'force_no_hermitian_symmetry': False, 'starting_scale': 2, 'ending_scale': 1, 'edge_width_for_prior': 2, 'num_throughput_iterations': 2, 'initial_throughput_estimate': 1, 'maximize_planet_throughput': True}, 'solver': {'num_threads': 0, 'crossover': 0, 'method': 2}}
+file_organization = {'survey_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex', 'solution_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/solutions', 'analysis_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/analysis', 'drivers_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/drivers', 'log_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/logs', 'input_files_dir': '/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/masks'}
+solution_fname = "/user/bnickson/git_hub/LUVex_TelAps/aplc_optimization/surveys/scda_LUVex_TestAps_survey_N0512_telserv3/5-Hex/solutions/0_SCDA_N512_FPM350M0150_IWA0340_OWA01200_C10_BW10_Nlam3_LS_IDex_ID_OD0_OD_ls_982_no_strut.fits"
+
+# TEMPLATE DRIVER SCRIPT
+# The driver script provides a record of the file organization and design parameters for a given survey
+# and is also used to call the optimizer.
+
+import os
+import pprint
+
+import asdf
+import numpy as np
+from astropy.io import fits
+from hcipy import *
+
+from aplc_optimization.optimizer import optimize_aplc
+
+print('Optimizer called with the following parameters:')
+pprint.pprint(parameters, width=1)
+print('')
+print('File organization:')
+pprint.pprint(file_organization, width=1)
+print('')
+
+# Getting values from the template
+# Driver is automatically written with "parameters" and "file_organization"
+pup_fname = parameters['pupil']['filename']
+fpm_radius = parameters['focal_plane_mask']['radius']
+fpm_num_pix = parameters['focal_plane_mask']['num_pix']
+fpm_grayscale = parameters['focal_plane_mask']['grayscale']
+ls_fname = parameters['lyot_stop']['filename']
+ls_alignment_tolerance = parameters['lyot_stop']['alignment_tolerance']
+ls_num_stops = parameters['lyot_stop']['num_lyot_stops']
+img_contrast = 10 ** (-parameters['image']['contrast'])
+img_iwa = parameters['image']['iwa']
+img_owa = parameters['image']['owa']
+img_num_wavelengths = parameters['image']['num_wavelengths']
+img_bandwidth = parameters['image']['bandwidth']
+img_resolution = parameters['image']['resolution']
+method_force_no_x_mirror_symmetry = parameters['method']['force_no_x_mirror_symmetry']
+method_force_no_y_mirror_symmetry = parameters['method']['force_no_y_mirror_symmetry']
+method_force_no_hermitian_symmetry = parameters['method']['force_no_hermitian_symmetry']
+method_starting_scale = parameters['method']['starting_scale']
+method_ending_scale = parameters['method']['ending_scale']
+method_edge_width_for_prior = parameters['method']['edge_width_for_prior']
+method_num_throughput_iterations = parameters['method']['num_throughput_iterations']
+method_initial_throughput_estimate = parameters['method']['initial_throughput_estimate']
+method_maximize_planet_throughput = parameters['method']['maximize_planet_throughput']
+solver_num_threads = parameters['solver']['num_threads']
+solver_crossover = parameters['solver']['crossover']
+solver_method = parameters['solver']['method']
+
+if not os.path.isabs(pup_fname):
+    pup_fname = os.path.join(file_organization['input_files_dir'], pup_fname)
+if not os.path.isabs(ls_fname):
+    ls_fname = os.path.join(file_organization['input_files_dir'], ls_fname)
+
+pupil = read_fits(pup_fname)
+
+num_pix = pupil.shape[0]
+pupil_grid = make_uniform_grid(num_pix, [1, 1])
+pupil = Field(pupil.ravel(), pupil_grid)
+
+# try:
+#   lyot_stops = [Field(read_fits(ls_fname.format(i)).ravel(), pupil_grid) for i in range(ls_num_stops)]
+# except:
+lyot_stop = Field(read_fits(ls_fname).ravel(), pupil_grid)
+
+# Building Lyot stops according to alignment tolerance
+if ls_num_stops in [1, 5, 9]:
+    lyot_stops = [lyot_stop]
+else:
+    lyot_stops = []
+
+if ls_num_stops in [4, 5, 8, 9]:
+    lyot_stop_pos_x = np.roll(lyot_stop.shaped, ls_alignment_tolerance, 1).ravel()
+    lyot_stop_neg_x = np.roll(lyot_stop.shaped, -ls_alignment_tolerance, 1).ravel()
+    lyot_stop_pos_y = np.roll(lyot_stop.shaped, ls_alignment_tolerance, 0).ravel()
+    lyot_stop_neg_y = np.roll(lyot_stop.shaped, -ls_alignment_tolerance, 0).ravel()
+
+    lyot_stops.extend([lyot_stop_pos_x, lyot_stop_neg_x, lyot_stop_pos_y, lyot_stop_neg_y])
+
+if ls_num_stops in [8, 9]:
+    lyot_stop_pos_x_pos_y = np.roll(np.roll(lyot_stop.shaped, ls_alignment_tolerance, 1), ls_alignment_tolerance,
+                                    0).ravel()
+    lyot_stop_pos_x_neg_y = np.roll(np.roll(lyot_stop.shaped, ls_alignment_tolerance, 1), -ls_alignment_tolerance,
+                                    0).ravel()
+    lyot_stop_neg_x_pos_y = np.roll(np.roll(lyot_stop.shaped, -ls_alignment_tolerance, 1), ls_alignment_tolerance,
+                                    0).ravel()
+    lyot_stop_neg_x_neg_y = np.roll(np.roll(lyot_stop.shaped, -ls_alignment_tolerance, 1), -ls_alignment_tolerance,
+                                    0).ravel()
+
+    lyot_stops.extend([lyot_stop_pos_x_pos_y, lyot_stop_pos_x_neg_y, lyot_stop_neg_x_pos_y, lyot_stop_neg_x_neg_y])
+
+# Build science focal grid
+n_sci = int((np.ceil(img_owa) + 1) * img_resolution) * 2
+x_sci = (np.arange(n_sci) + 0.5 - n_sci / 2) / img_resolution
+focal_grid = CartesianGrid(SeparatedCoords((x_sci, x_sci)))
+
+dark_zone_mask = circular_aperture(img_owa * 2)(focal_grid) - circular_aperture(img_iwa * 2)(focal_grid)
+
+# Build focal plane mask
+q_foc = fpm_num_pix / (fpm_radius * 2)
+x_foc = (np.arange(fpm_num_pix) + 0.5 - fpm_num_pix / 2) / q_foc
+focal_mask_grid = CartesianGrid(RegularCoords(1.0 / q_foc, [fpm_num_pix, fpm_num_pix], x_foc.min()))
+
+if fpm_grayscale:
+    focal_plane_mask = 1 - evaluate_supersampled(circular_aperture(2 * fpm_radius), focal_mask_grid, 8)
+else:
+    focal_plane_mask = 1 - circular_aperture(2 * fpm_radius)(focal_mask_grid)
+
+if img_num_wavelengths == 1:
+    wavelengths = [1]
+else:
+    wavelengths = np.linspace(-img_bandwidth / 2, img_bandwidth / 2, img_num_wavelengths) + 1
+
+# Optimize
+apodizer = optimize_aplc(
+    pupil=pupil,
+    focal_plane_mask=focal_plane_mask,
+    lyot_stops=lyot_stops,
+    dark_zone_mask=dark_zone_mask,
+    wavelengths=wavelengths,
+    contrast=img_contrast,
+    starting_scale=method_starting_scale,
+    ending_scale=method_ending_scale,
+    force_no_x_symmetry=method_force_no_x_mirror_symmetry,
+    force_no_y_symmetry=method_force_no_y_mirror_symmetry,
+    force_no_hermitian_symmetry=method_force_no_hermitian_symmetry,
+    maximize_planet_throughput=method_maximize_planet_throughput,
+    num_throughput_iterations=method_num_throughput_iterations,
+    initial_throughput_estimate=method_initial_throughput_estimate,
+    edge_width_for_prior=method_edge_width_for_prior,
+    solver_num_threads=solver_num_threads,
+    solver_crossover=solver_crossover,
+    solver_method=solver_method)
+
+# TODO: units and descriptions for head keywords
+# build fits cube? possible to do with asdf format?
+
+# fits header
+hdr = fits.header.Header()
+hdr.set('PUP', parameters['pupil']['filename'], 'pupil filename')
+hdr.set('FPM_RAD', fpm_radius, 'lam/d')
+hdr.set('FPM_PIX', fpm_num_pix, '# pix across FPM')
+hdr.set('FPM_GY', fpm_grayscale, 'is FPM greyscale?')
+hdr.set('LS', parameters['lyot_stop']['filename'], 'LS filename')
+hdr.set('LS_ALIGN_TOl', ls_alignment_tolerance, 'pix of ls alignment tolerance')
+hdr.set('LS_NUM_STOPS', ls_num_stops, '# of LS shifts')
+hdr.set('CONTRAST', img_contrast, 'contrast in darkzone')
+hdr.set('IWA', img_iwa, 'lam/D')
+hdr.set('OWA', img_owa, 'lam/D')
+hdr.set('NLAM', img_num_wavelengths)
+hdr.set('BW', img_bandwidth)
+hdr.set('IMG_RES', img_resolution, 'image resolution')
+hdr.set('PROG_START', method_starting_scale, 'starting scale for progressive')
+hdr.set('PROG_END', method_ending_scale, 'ending scale for progressive')
+hdr.set('PROG_EDGE', method_edge_width_for_prior, 'edge width for progressive')
+hdr.set('THRPT_ITER', method_num_throughput_iterations, '# of throughput interatations')
+hdr.set('THRPT_ESTIM', method_initial_throughput_estimate, 'initial throughput estim')
+hdr.set('MAX_THRPT', method_maximize_planet_throughput, 'is throughput maximized?')
+hdr.set('SLV_THRDS', solver_num_threads, '# solver threads')
+hdr.set('SLV_XOVER', solver_crossover, 'crossover type')
+hdr.set('SLV_MTHD', solver_method, 'solver method')
+
+# Write to file
+hdu_list = fits.HDUList()
+hdu_list.append(fits.ImageHDU((apodizer * (pupil > 0)).shaped, header=hdr, name='APOD'))
+hdu_list.append(fits.ImageHDU(pupil.shaped, name='PUPIL'))
+hdu_list.append(fits.ImageHDU(lyot_stops[0].shaped, name='LYOT STOP'))
+
+tree = {'parameters': parameters, 'file_organization': file_organization, 'apodizer': hdu_list['APOD'].data,
+        'pupil': hdu_list['PUPIL'].data, 'lyot stop': hdu_list['LYOT STOP'].data}
+
+ff = asdf.fits_embed.AsdfInFits(hdu_list, tree)
+ff.write_to(solution_fname, overwrite=True)
